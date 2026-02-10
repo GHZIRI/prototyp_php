235279d52e81d559f638e65186838dce544a7c69
GHZIRI <salmantanger1234@gmail.com>
2026-02-10 11:23:37 +0000

    modify inde.php

--- index.php
+++ index.php
@@ -6,24 +6,30 @@
     <title>Document</title>
 </head>
 <body>
-    <table border="2">
+    <?php
+    $a = 0;
+    $f_name =$_POST ["first_name"];
+   if($_POST["first_name"]  == "" || $_POST["last_name"] == "" || $_POST["adrisse"] || $_POST["city"] || $_POST["code_cline"] ){
+        
+        echo  "please check if the formul is full now "; 
+        return;
+
+   }
+    ?>
+    <table border = "2">
         <tr>
             <th>champ</th>
-            <th>valeur</th>
+            <th>value</th>
         </tr>
-        
-            
-        
         <tr>
             <td>first name</td>
-            <td><?php echo $_POST["first_name"];?></td>
+            <td><?php echo $_POST ["first_name"];?></td>
+        </tr>
+        <td>last name</td>
+        <td><?php echo $_POST ["last_name"];?><td>
         </tr>
         <tr>
-            <td>last name</td>
-            <td><?php echo $_POST["last_name"];?></td>
-        </tr>
-        <tr>
-            <td>drisse</td>
+            <td>Adrisse</td>
             <td><?php echo $_POST["adrisse"];?></td>
         </tr>
         <tr>
@@ -31,10 +37,9 @@
             <td><?php echo $_POST["city"];?></td>
         </tr>
         <tr>
-            <td>code_cline</td>
-             <td><?php echo $_POST["code_cline"];?></td>
-        </trt>
-
+            <td>code cline</td>
+            <td><?php echo $_POST["code_cline"];?> </td>
+        </tr>
     </table>
 </body>
 </html>
